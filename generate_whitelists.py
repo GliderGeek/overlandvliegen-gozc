7fd1ebc3ebf294fbbc187838094447503d69564e
glidergeek <GliderGeek@users.noreply.github.com>
2022-05-16 21:24:47 +0200

    cleanup

--- generate_whitelists.py
+++ generate_whitelists.py
@@ -1,6 +1,16 @@
+
+"""
+Generate whitelists from planes.csv to filter Gooise airplanes on various tracking sites.
+Generates files next to script:
+- live.glidernet (glidernet.OGN)
+- glidertracker.de (glidertracker.csv)
+- glideandseek.com (glideandseek.json)
+"""
+
 import csv
 import json
 from dataclasses import dataclass
+
 
 @dataclass
 class Plane:
@@ -9,15 +19,23 @@ class Plane:
     registration: str
     flarm_id: str
 
-wlist = []
+
 planes = []
 with open('planes.csv', newline='') as csvfile:
     for plane in csv.DictReader(csvfile, delimiter=','):
         planes.append(Plane(plane['TYPE'], plane['CN'], plane['CALL'], plane['ID'][2::]))
-        wlist.append(plane['ID'][2::])
 
 
-def serialize_for_glide_and_seek(plane: Plane) -> dict:
+def serialize_glidertracker(plane: Plane) -> dict:
+    return {
+        "ID": f"06{plane.flarm_id}",
+        "CALL": plane.registration,
+        "CN": plane.competition_id,
+        "TYPE": plane.plane_type,
+    }
+
+
+def serialize_glideandseek(plane: Plane) -> dict:
     return {
         "cn": plane.competition_id,
         "glider": plane.plane_type,
@@ -25,25 +43,32 @@ def serialize_for_glide_and_seek(plane: Plane) -> dict:
         "name": plane.registration,  # TODO: owner? necessarily unique?
     }
 
-with open('glide_and_seek.json', 'w') as f:
+
+def write_glidernet(planes, f):
+    wlist = [plane.flarm_id for plane in planes]
+    export_dict = {
+        "tasks": [{
+            "name": "GoZC",
+            "color": "0080FF",
+            "wlist": wlist,
+            "legs": []
+        }]
+    }
+
+    json.dump(export_dict, f, indent=1)
+
+
+with open('glideandseek.json', 'w') as f:
     serialized = []
     for plane in planes:
-        serialized.append(serialize_for_glide_and_seek(plane))
+        serialized.append(serialize_glideandseek(plane))
     json.dump(serialized, f)
 
+with open('glidertracker.csv', 'w', newline='') as f:
+    serialized_planes = [serialize_glidertracker(plane) for plane in planes]
+    writer = csv.DictWriter(f, serialized_planes[0].keys())
+    writer.writeheader()
+    writer.writerows(serialized_planes)
 
-
-
-# export_dict = {
-#   "tasks": [
-#     {
-#       "name": "GoZC",
-#       "color": "0080FF",
-#       "wlist": wlist,
-#       "legs": []
-#     }
-#   ]
-# }
-
-# with open('task.OGN', 'w') as f:
-#     json.dump(export_dict, f, indent=1)
+with open('glidernet.OGN', 'w') as f:
+    write_glidernet(planes, f)
